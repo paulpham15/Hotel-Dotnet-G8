975dbcb35767d324051d2c5a02416ce7c5dd4acb
Paul <paul@vinova.com.sg>
2023-05-28 11:38:06 +0700

    user dropdown

--- HotelDotNet/wwwroot/js/site.js
+++ HotelDotNet/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(".user-menu").click(function () {
+    $(this).toggleClass("show");
+});

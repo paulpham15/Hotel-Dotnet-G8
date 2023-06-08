2c754cc5f4c97945f3ed5bc1bb4e6801ff3ed696
TrHoangLam <92965175+TrHoangLam@users.noreply.github.com>
2023-06-08 15:07:51 +0700

    login

--- HotelDotNet/wwwroot/js/site.js
+++ HotelDotNet/wwwroot/js/site.js
@@ -7,7 +7,27 @@ var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0');
 var yyyy = today.getFullYear();
+function show() {
+    var p = document.getElementById('pwd');
+    p.setAttribute('type', 'text');
+}
 
+function hide() {
+    var p = document.getElementById('pwd');
+    p.setAttribute('type', 'password');
+}
+
+var pwShown = 0;
+
+document.getElementById("eye").addEventListener("click", function () {
+    if (pwShown == 0) {
+        pwShown = 1;
+        show();
+    } else {
+        pwShown = 0;
+        hide();
+    }
+}, false);
 
 today = yyyy + '-' + mm + '-' + dd;
 
